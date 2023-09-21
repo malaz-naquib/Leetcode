67f422977d849b92610c6357ade5635e1b4a0f0f
Malaz Naquib <malaz.naquib1@gmail.com>
2023-09-21 13:29:22 -0700

    Completed daily leetcode problem

---
+++ Medium/insert-interval.js
@@ -0,0 +1,60 @@
+// You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent the start and the end of the ith interval and intervals is sorted in ascending order by starti. You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
+
+// Insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
+
+// Return intervals after the insertion.
+
+ 
+
+// Example 1:
+
+// Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+// Output: [[1,5],[6,9]]
+// Example 2:
+
+// Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+// Output: [[1,2],[3,10],[12,16]]
+// Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+ 
+
+// Constraints:
+
+// 0 <= intervals.length <= 104
+// intervals[i].length == 2
+// 0 <= starti <= endi <= 105
+// intervals is sorted by starti in ascending order.
+// newInterval.length == 2
+// 0 <= start <= end <= 105
+
+/**
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
+ */
+
+var insert = function(intervals, newInterval) {
+  let res = [];
+  let i = 0;
+
+  const start = 0;
+  const end = 1;
+
+  while(i < intervals.length && intervals[i][end] < newInterval[start]) {
+    res.push(intervals[i]);
+    i++;
+  }
+
+  while(i < intervals.length && intervals[i][start] <= newInterval[end]) {
+    newInterval[start] = Math.min(newInterval[start], intervals[i][start]);
+    newInterval[end] = Math.max(newInterval[end], intervals[i][end]);
+    i++;
+  }
+
+  res.push(newInterval);
+
+  while(i < intervals.length) {
+    res.push(intervals[i]);
+    i++;
+  }
+  return res;
+};
