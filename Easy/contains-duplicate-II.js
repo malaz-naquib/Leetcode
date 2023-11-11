87d151598ba8bd288284bd1230e6f57b80aaf6d8
Malaz Naquib <malaz.naquib1@gmail.com>
2023-11-11 14:46:32 -0800

    Completed daily leetcode problem

---
+++ Easy/contains-duplicate-II.js
@@ -0,0 +1,33 @@
+// Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+
+// Example 1:
+
+// Input: nums = [1,2,3,1], k = 3
+// Output: true
+// Example 2:
+
+// Input: nums = [1,0,1,1], k = 1
+// Output: true
+// Example 3:
+
+// Input: nums = [1,2,3,1,2,3], k = 2
+// Output: false
+ 
+// Constraints:
+
+// 1 <= nums.length <= 105
+// -109 <= nums[i] <= 109
+// 0 <= k <= 105
+
+var containsNearbyDuplicate = function(nums, k) {
+    const hash = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        if (i - hash.get(nums[i]) <= k) {
+            return true;
+        }
+        hash.set(nums[i], i);
+    }
+    return false;
+};
+// Time Complexity: O(n)
+// Space Complexity: O(n)
