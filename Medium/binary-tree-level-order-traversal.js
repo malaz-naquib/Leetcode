22cb62ca9d700c830d96589ca79f49ad29fbf5bc
Malaz Naquib <malaz.naquib1@gmail.com>
2023-12-04 15:58:47 -0800

    Completed daily leetcode problem

---
+++ Medium/binary-tree-level-order-traversal.js
@@ -0,0 +1,34 @@
+// Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+
+// Example 1:
+
+// Input: root = [3,9,20,null,null,15,7]
+// Output: [[3],[9,20],[15,7]]
+// Example 2:
+
+// Input: root = [1]
+// Output: [[1]]
+// Example 3:
+
+// Input: root = []
+// Output: []
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [0, 2000].
+// -1000 <= Node.val <= 1000
+
+var levelOrder = function(root) {
+    let q = [root], ans = []
+    while (q[0]) {
+        let qlen = q.length, row = []
+        for (let i = 0; i < qlen; i++) {
+            let curr = q.shift()
+            row.push(curr.val)
+            if (curr.left) q.push(curr.left)
+            if (curr.right) q.push(curr.right)
+        }
+        ans.push(row)            
+    }
+    return ans
+};
